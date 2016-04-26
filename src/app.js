68132ee10f96fea3efa2c9d9f3bd64b5064669c2
Mateusz Bosek <bosek.mateusz@gmail.com>
2016-04-26 19:30:13 +0200

    Add new layout

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 import "./../resources/styles/main.sass";
 import {config as routesConfig} from "./configs/routes";
 import {SocketComponent} from "./components/SocketComponent";
